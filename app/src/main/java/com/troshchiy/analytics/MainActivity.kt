605a459dbf7bb5cb8d170ee28994e202c6346b7a
yvgen.troshchiy <yvgen.troshchiy@gmail.com>
2017-06-28 18:42:55 +0300

    Add tracker?.send

--- app/src/main/java/com/troshchiy/analytics/MainActivity.kt
+++ app/src/main/java/com/troshchiy/analytics/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.troshchiy.analytics
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.analytics.HitBuilders
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tracker = App.APP.getDefaultTracker()
+        tracker?.setScreenName("MainActivity")
+        tracker?.send(HitBuilders.ScreenViewBuilder().build())
+
+        tracker?.send(HitBuilders.EventBuilder()
+                .setCategory("Action")
+                .setAction("Share")
+                .build())
     }
 }
