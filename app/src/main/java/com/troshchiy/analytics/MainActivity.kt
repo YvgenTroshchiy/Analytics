7a6c706b0cfc71f7aad017029cd9590286a4936b
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-07-20 01:51:20 +0300

    Update activity_main.xml

--- app/src/main/java/com/troshchiy/analytics/MainActivity.kt
+++ app/src/main/java/com/troshchiy/analytics/MainActivity.kt
@@ -3,6 +3,7 @@ package com.troshchiy.analytics
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.analytics.HitBuilders
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tracker = App.APP.getDefaultTracker()
-        tracker?.setScreenName("MainActivity")
-        tracker?.send(HitBuilders.ScreenViewBuilder().build())
+        btn_a.setOnClickListener { }
+        btn_b.setOnClickListener { }
+        btn_c.setOnClickListener { }
 
-        tracker?.send(HitBuilders.EventBuilder()
+        sendAnalyticsData()
+    }
+
+    private fun sendAnalyticsData() {
+        App.tracker.setScreenName("MainActivity Screen")
+        App.tracker.send(HitBuilders.ScreenViewBuilder().build())
+
+        App.tracker.send(HitBuilders.EventBuilder()
                 .setCategory("Category")
                 .setAction("Action")
                 .build())
