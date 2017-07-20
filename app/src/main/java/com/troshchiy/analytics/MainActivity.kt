63d051ed969a6e8fac90b2f1c74cd0be49aee5ba
YevhenTroshciy <yvgen.troshchiy@gmail.com>
2017-07-20 14:08:46 +0300

    Add sendFirebaseEvent

--- app/src/main/java/com/troshchiy/analytics/MainActivity.kt
+++ app/src/main/java/com/troshchiy/analytics/MainActivity.kt
@@ -3,18 +3,40 @@ package com.troshchiy.analytics
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.analytics.HitBuilders
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_a.setOnClickListener { sendAnalyticsEvent("A") }
-        btn_b.setOnClickListener { sendAnalyticsEvent("B") }
-        btn_c.setOnClickListener { sendAnalyticsEvent("C") }
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        btn_a.setOnClickListener {
+            sendFirebaseEvent("Firebase A")
+            sendAnalyticsEvent("Analytics A")
+        }
+        btn_b.setOnClickListener {
+            sendFirebaseEvent("Firebase B")
+            sendAnalyticsEvent("Analytics B")
+        }
+        btn_c.setOnClickListener {
+            sendFirebaseEvent("Firebase C")
+            sendAnalyticsEvent("Analytics C")
+        }
+    }
+
+    private fun sendFirebaseEvent(name: String) {
+        val bundle = Bundle()
+//        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id)
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name)
+//        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "type")
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 
     private fun sendAnalyticsEvent(name: String) {
@@ -26,6 +48,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        sendFirebaseEvent("Firebase MainActivity Screen")
         sendAnalyticsData()
     }
 
